cce93aa62c2aa7693929a083d74f457d57bab375
Sameer <k200439@nu.edu.pk>
2023-09-13 20:47:03 +0500

    Integrated sign out functionality, integrated load url and scrape url with the backend

--- client/src/components/scrape/Load_url.jsx
+++ client/src/components/scrape/Load_url.jsx
@@ -1,24 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Source_1_2_card from './child/Source_1_2_card'
 import Heading_text from '../shared_components/Heading_text'
 import Input_field from '../shared_components/Input_field'
 import Button from '../shared_components/Button'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import serverBasePath from '../../../constants'
 
 export default function Load_url() {
 
-    
+  const [baseLink, setBaseLink] = useState('');
+  // const [sources, setSources] = useState([]);
+  const navigate = useNavigate();
+  let chatbotId;
+
+
+
+  function getLinks() {
+
+    if (chatbotId === undefined) {
+      axios.get(serverBasePath + '/new-chatbot', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        withCredentials: true
+      })
+        .then(response => {
+          const data = response.data;
+          console.log(data)
+          chatbotId = data.newBotId;
+        })
+        .catch(err => console.log(err));
+    }
+
+
+    axios.post(serverBasePath + '/train/website/baseLink', { url: baseLink }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      withCredentials: true
+    })
+      .then((response) => {
+        const data = response.data;
+        if (data.pagesData !== undefined) {
+          data.pagesData.forEach((link, index) => {
+            if (link.id === undefined) {
+              link.id = index;
+            }
+          })
+          // setSources(data.pagesData);
+          navigate('/scrape', { state: { sources: data.pagesData, chatbotId: chatbotId } })
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
 
   return (
     <>
-        <Source_1_2_card/>
-        <div className='flex flex-col gap-3 justify-center items-center text-center'>
-            <Heading_text text_size={"text-xl sm:text-3xl mt-4 font-bold text-gray-800"} text={"Add a Website as Data Source"}/>
-            <Heading_text text_size={"text-md text-gray-800"} text={"Add a website as a data source by providing a URL for LiveChatAI to crawl and import content."}/>
-            <Heading_text text_size={"text-sm mb-3 text-gray-500"} text={"Use the main domain (e.g., domain.com) to crawl all pages, or specify a sub-path (e.g., domain.com/help/) or subdomain (e.g., docs.domain.com) to limit the content imported. This helps in tailoring the AI bot's training to your specific needs."}/>
-            <Input_field name={"url"} placeholder={"https://..."} style={"border-2 outline-main w-[94vw] sm:w-[45vw] pl-2 h-10 rounded-md"}/>
-            <Button style={"bg-gray-800 text-white p-3 pl-7 pr-7 rounded-full active:scale-95"} text={"Save and get all my links"}/>
-            
-            <div className='flex w-full gap-2 justify-center items-center'>
+      <Source_1_2_card />
+      <div className='flex flex-col gap-3 justify-center items-center text-center'>
+        <Heading_text text_size={"text-xl sm:text-3xl mt-4 font-bold text-gray-800"} text={"Add a Website as Data Source"} />
+        <Heading_text text_size={"text-md text-gray-800"} text={"Add a website as a data source by providing a URL for LiveChatAI to crawl and import content."} />
+        <Heading_text text_size={"text-sm mb-3 text-gray-500"} text={"Use the main domain (e.g., domain.com) to crawl all pages, or specify a sub-path (e.g., domain.com/help/) or subdomain (e.g., docs.domain.com) to limit the content imported. This helps in tailoring the AI bot's training to your specific needs."} />
+        <Input_field
+          name={"url"}
+          placeholder={"https://www.example.com"}
+          style={"border-2 outline-main w-[94vw] sm:w-[45vw] pl-2 h-10 rounded-md"}
+          value={baseLink}
+          setValue={setBaseLink}
+        />
+        <Button style={"bg-gray-800 text-white p-3 pl-7 pr-7 rounded-full active:scale-95"} text={"Save and get all my links"}
+          action={getLinks}
+        />
+
+        {/* <div className='flex w-full gap-2 justify-center items-center'>
                 <div className='w-[25%] bg-gray-400 h-[1px]'></div>
                 <h3>OR</h3>
                 <div className='w-[25%] bg-gray-400 h-[1px]'></div>
@@ -27,9 +85,9 @@ export default function Load_url() {
             <Heading_text text_size={"text-xl md:text-4xl mt-4 font-bold text-gray-800"} text={"Submit Sitemap"}/>
             <Heading_text text_size={"text-md text-gray-800"} text={"When you add sitemap, we only crawl and get content from provided URLs on your sitemap."}/>
             <Input_field name={"sitemap"} placeholder={"https://..../sitemap.xml"} style={"border-2 w-[94vw] outline-main sm:w-[45vw] pl-2 h-10 rounded-md"}/>
-            <Button style={"bg-gray-800 mb-14 text-white p-3 pl-7 pr-7 rounded-full active:scale-95"} text={"Save and load sitemap"}/>
-            
-        </div>
+            <Button style={"bg-gray-800 mb-14 text-white p-3 pl-7 pr-7 rounded-full active:scale-95"} text={"Save and load sitemap"}/> */}
+
+      </div>
     </>
   )
 }
--- client/src/components/shared_components/Scraped_link.jsx
+++ client/src/components/shared_components/Scraped_link.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import {RiDeleteBin6Line} from "react-icons/ri"
+import { RiDeleteBin6Line } from "react-icons/ri"
 
-export default function Scraped_link({link,data}) {
-  return (
-    <>
-        <div className='flex items-center justify-center gap-6'>
-            <div>
-                <input className='border-2 outline-none rounded-md w-full sm:w-[38vw] text-gray-600 h-10 pl-2' type="text" name="links" value={link}/>
+export default function Scraped_link({ link, data, id, deleteAction}) {
+    return (
+        <>
+            <div className='flex items-center justify-center gap-6'>
+                <div>
+                    <input className='border-2 outline-none rounded-md w-full sm:w-[38vw] text-gray-600 h-10 pl-2' type="text" name="links" value={link} />
+                </div>
+                <div>
+                    <h3>{data}</h3>
+                </div>
+                <div onClick={() => {deleteAction(id) }}>
+                    <RiDeleteBin6Line className='text-2xl hover:text-red-400 active:scale-90 text-gray-500 cursor-pointer' />
+                </div>
             </div>
-            <div>
-                <h3>{data}</h3>
-            </div>
-            <div>
-                <RiDeleteBin6Line className='text-2xl hover:text-red-400 active:scale-90 text-gray-500 cursor-pointer'/>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
