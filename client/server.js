357a5512d8c14c51e2b99c0605f0645e006a8e45
Sameer <sameer.raza9200@gmail.com>
2023-09-15 02:19:01 +0500

    Trying to deploy

--- client/server.js
+++ client/server.js
@@ -1,7 +1,11 @@
 import path from 'path';
 import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 app.use(express.static(path.join(__dirname, '.', 'build')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
