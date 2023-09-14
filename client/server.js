bcd222e1ae370d9650dba2bd737379d461a87751
Sameer <sameer.raza9200@gmail.com>
2023-09-15 02:22:48 +0500

    fixing the front end servers path

--- client/server.js
+++ client/server.js
@@ -6,9 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-app.use(express.static(path.join(__dirname, '.', 'build')));
+app.use(express.static(path.join(__dirname, '.', 'dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
