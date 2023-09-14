ce6ac0ba92c17bf16f3e6171259bd578faa0d6c8
Sameer <k200439@nu.edu.pk>
2023-09-15 00:04:45 +0500

    Made single link part of website page work with backend. Rest of it is not ready

--- client/src/components/dashboard/manage_data_sources/Website.jsx
+++ client/src/components/dashboard/manage_data_sources/Website.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Input_field from '../../shared_components/Input_field'
-import { MdDeleteOutline } from "react-icons/md"
-import { LuRefreshCw } from "react-icons/lu"
 import Button from '../../shared_components/Button'
+import IncludedLink from './childs/IncludedLink'
+import axios from 'axios'
+import serverBasePath from '../../../../constants'
+import { useParams } from 'react-router-dom'
 
 export default function Website() {
+  const [links, editLinks] = useState([]);
+  const [singleLink, editSingleLink] = useState('');
+  const { id } = useParams();
+
+  function addLink(link, index) {
+    editLinks(prev => {
+      if (link.id === undefined) {
+        link.id = index
+      }
+      return [...prev, link]
+    })
+  }
+
+  function getLinks() {
+    axios.get(serverBasePath + '/train/website/' + id, {
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+      },
+      withCredentials: true
+    })
+      .then((response) => {
+        const data = response.data;
+        if (Object.keys(data).length !== 0) {
+          data.links.map((link, index) => addLink(link, index))
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  function sendLinks(untrainedLinks) {
+    let single = true;
+    if (untrainedLinks === undefined) {
+      single= false;
+      untrainedLinks = links.filter(item => item.status === undefined || item.status === '');
+    }
+
+    axios.post(serverBasePath + '/train/website/links',
+      { links: untrainedLinks, chatbotId: id },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        withCredentials: true
+      }
+    )
+      .then((response) => {
+        const data = response.data;
+        if (Object.keys(data).length !== 0 && response.status !== 400) {
+          editLinks([]);
+          data.links.map((link, index) => addLink(link, index));
+          if (single){
+            editSingleLink('');
+          }
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  function trainSingleLink() {
+    const linkToCheck = links.filter(link => link.link === singleLink);
+    if (linkToCheck.length === 0) {
+      sendLinks([{link: singleLink}]);
+    }
+  }
+
+  useEffect(getLinks, []);
+
   return (
     <>
       <div className='flex items-center flex-col gap-4'>
@@ -23,10 +94,19 @@ export default function Website() {
 
       <div className='flex flex-col gap-4'>
 
-        <h3 className='text-3xl font-bold'>Crawl a new website</h3>
+        <h3 className='text-3xl font-bold'>Crawl a single URL</h3>
+
         <div className='flex gap-2 items-center'>
-          <Input_field placeholder={"Enter Domain"} style={"border-2 rounded-md w-[30vw] h-[6vh] pl-2"} />
-          <Button style={"bg-gray-800 text-white p-2 pl-3 pr-3 rounded-md"} text={"Fetch new links from domain"} />
+
+          <Input_field
+            placeholder={"https://www.example.com"}
+            style={"border-2 rounded-md w-[30vw] h-[6vh] pl-2"}
+            value={singleLink}
+            setValue={editSingleLink}
+            name='domain'
+          />
+
+          <Button style={"bg-gray-800 text-white p-2 pl-3 pr-3 rounded-md"} text={"Fetch new links from domain"} action={trainSingleLink}/>
         </div>
       </div>
       {/* -------------------Imported & Trained Web Pages------------------- */}
@@ -44,82 +124,15 @@ export default function Website() {
 
 
         {/* --------------------from here trained url------------------------------------- */}
-        <div className='w-[50vw] gap-2 flex items-center justify-between'>
-          <div className='flex w-[40vw] border-[1px] rounded-md p-2 pl-4 pr-4 border-gray-800 items-center justify-between'>
-            <div><h3>https://studyloverveer.in/feed</h3></div>
-            <div className='bg-blue-100 rounded-md p-[2px] pl-3 pr-3'><h3 className='text-sm'>Trained</h3></div>
-          </div>
 
-          <div><h3>345</h3></div>
-          <div className='cursor-pointer active:scale-95 hover:text-red-500'>
-            <MdDeleteOutline className='text-2xl' />
-          </div>
-          <div className='cursor-pointer active:scale-95'>
-            <LuRefreshCw className='text-2xl' />
-          </div>
+        {
+          links.map(webpage => <IncludedLink
+            link={webpage}
+          />)
+        }
 
-        </div>
 
-        <div className='w-[50vw] gap-2 flex items-center justify-between'>
-          <div className='flex w-[40vw] border-[1px] rounded-md p-2 pl-4 pr-4 border-gray-800 items-center justify-between'>
-            <div><h3>https://studyloverveer.in/feed</h3></div>
-            <div className='bg-blue-100 rounded-md p-[2px] pl-3 pr-3'><h3 className='text-sm'>Trained</h3></div>
-          </div>
 
-          <div><h3>345</h3></div>
-          <div className='cursor-pointer active:scale-95 hover:text-red-500'>
-            <MdDeleteOutline className='text-2xl' />
-          </div>
-          <div className='cursor-pointer active:scale-95'>
-            <LuRefreshCw className='text-2xl' />
-          </div>
-
-        </div>
-        <div className='w-[50vw] gap-2 flex items-center justify-between'>
-          <div className='flex w-[40vw] border-[1px] rounded-md p-2 pl-4 pr-4 border-gray-800 items-center justify-between'>
-            <div><h3>https://studyloverveer.in/feed</h3></div>
-            <div className='bg-blue-100 rounded-md p-[2px] pl-3 pr-3'><h3 className='text-sm'>Trained</h3></div>
-          </div>
-
-          <div><h3>345</h3></div>
-          <div className='cursor-pointer active:scale-95 hover:text-red-500'>
-            <MdDeleteOutline className='text-2xl' />
-          </div>
-          <div className='cursor-pointer active:scale-95'>
-            <LuRefreshCw className='text-2xl' />
-          </div>
-
-        </div>
-        <div className='w-[50vw] gap-2 flex items-center justify-between'>
-          <div className='flex w-[40vw] border-[1px] rounded-md p-2 pl-4 pr-4 border-gray-800 items-center justify-between'>
-            <div><h3>https://studyloverveer.in/feed</h3></div>
-            <div className='bg-blue-100 rounded-md p-[2px] pl-3 pr-3'><h3 className='text-sm'>Trained</h3></div>
-          </div>
-
-          <div><h3>345</h3></div>
-          <div className='cursor-pointer active:scale-95 hover:text-red-500'>
-            <MdDeleteOutline className='text-2xl' />
-          </div>
-          <div className='cursor-pointer active:scale-95'>
-            <LuRefreshCw className='text-2xl' />
-          </div>
-
-        </div>
-        <div className='w-[50vw] gap-2 flex items-center justify-between'>
-          <div className='flex w-[40vw] border-[1px] rounded-md p-2 pl-4 pr-4 border-gray-800 items-center justify-between'>
-            <div><h3>https://studyloverveer.in/feed</h3></div>
-            <div className='bg-blue-100 rounded-md p-[2px] pl-3 pr-3'><h3 className='text-sm'>Trained</h3></div>
-          </div>
-
-          <div><h3>345</h3></div>
-          <div className='cursor-pointer active:scale-95 hover:text-red-500'>
-            <MdDeleteOutline className='text-2xl' />
-          </div>
-          <div className='cursor-pointer active:scale-95'>
-            <LuRefreshCw className='text-2xl' />
-          </div>
-
-        </div>
 
 
       </div>
