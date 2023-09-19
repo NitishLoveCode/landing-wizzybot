3f1daf16bcdc9b89a0dadad7f63bef6a6f2082a5
Nitish kumar <nitishlovecode@gmail.com>
2023-09-19 19:02:35 +0530

    now cancel button working saved questation

--- client/src/components/dashboard/manage_data_sources/Saved_question.jsx
+++ client/src/components/dashboard/manage_data_sources/Saved_question.jsx
@@ -35,7 +35,7 @@ export default function Saved_question({cur}) {
                         <textarea value={cur.question} className="border-[1px] outline-none border-gray-300 w-full p-2 rounded-md" placeholder='Answer' name="answer" cols="30" rows="5"></textarea>
                     </div>
                     <div className='flex justify-end gap-10 mt-2'>
-                        <Button style={"border-[1px] rounded-md border-gray-600 active:scale-95 w-fit p-2 pr-8 pl-8"} text={"Cancel"}/>
+                        <Button action={open_card} style={"border-[1px] rounded-md border-gray-600 active:scale-95 w-fit p-2 pr-8 pl-8"} text={"Cancel"}/>
                         <Button style={"border-[1px] bg-black text-white active:scale-95 rounded-md border-gray-800 w-fit p-2 pr-8 pl-8"} text={"Save"}/>
                     </div>
                 </div>
