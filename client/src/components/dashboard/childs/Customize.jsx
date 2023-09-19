d911c835dcd570362fe2257c3123cc6a89408705
Nitish kumar <nitishlovecode@gmail.com>
2023-09-19 23:18:58 +0530

    curtomize hidden card

--- client/src/components/dashboard/childs/Customize.jsx
+++ client/src/components/dashboard/childs/Customize.jsx
@@ -9,6 +9,7 @@ import serverBasePath from '../../../../constants';
 export default function Customize() {
 
   const { id } = useParams();
+  const [hidden, sethidden] = useState(false)
   const [settings, setSettings] = useState({});
   const [response, setResponse] = useState('');
   const [backup, setBackup] = useState({});//keeps a backup of the settings currently on the server.
@@ -68,6 +69,14 @@ export default function Customize() {
     const backupCopy = { ...backup };
     setSettings(backupCopy);
 
+  }
+
+  const hide_and_show=()=>{
+    if(hidden===true){
+      sethidden(false)
+    }else{
+      sethidden(true)
+    }
   }
 
   return (
@@ -179,13 +188,18 @@ export default function Customize() {
           </div>
 
           {/* ------------------------bottom card-------------- */}
-          <div className='border-[1px] border-gray-400 rounded-md gap-4 pb-4 w-full sm:w-[55vw]'>
-            <div className='flex h-14 cursor-pointer border-gray-400 border-b-[1px] px-5 items-center justify-between'>
-              <h3>Initial Message</h3>
-              <AiOutlineArrowDown />
-            </div>
 
-            <div className='px-5 mt-4 mb-4'>
+
+          {
+
+            hidden ? <>
+            <div className='border-[1px] border-gray-400 rounded-md gap-4 pb-4 w-full sm:w-[55vw]'>
+              <div onClick={()=>hide_and_show()} className='flex h-14 cursor-pointer border-gray-400 border-b-[1px] px-5 items-center justify-between'>
+                <h3>Initial Message</h3>
+                <AiOutlineArrowDown />
+              </div>
+
+              <div className='px-5 mt-4 mb-4'>
               <div>
                 <h3>Initial Message</h3>
               </div>
@@ -202,18 +216,6 @@ export default function Customize() {
               </div>
             </div>
 
-            {/* <div className='px-5 mt-4 mb-4'>
-              <div>
-                <h3>Initial Message</h3>
-              </div>
-              <div>
-                <textarea className='w-full px-2 outline-none h-32 rounded-md border-[1px] border-gray-400' name="Initial_Message"></textarea>
-              </div>
-              <div>
-                <h3 className='text-sm'>Enter each message in a new line.</h3>
-              </div>
-            </div> */}
-
             <div className='flex justify-end px-5 gap-4'>
               <button
                 className='border-[1px] border-gray-400 p-2 px-8 rounded-md active:scale-95'
@@ -229,6 +231,15 @@ export default function Customize() {
               </button>
             </div>
           </div>
+            </>:<>
+
+            <div onClick={()=>hide_and_show()} className='flex h-14 cursor-pointer border-gray-400 rounded-md border-[1px] px-5 items-center justify-between'>
+                <h3>Initial Message</h3>
+                <AiOutlineArrowDown />
+          </div>
+            </>
+          }
+
         </div>
 
         {
