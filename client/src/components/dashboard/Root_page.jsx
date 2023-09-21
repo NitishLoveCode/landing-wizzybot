7a090456887e04a2e2bf99b516dc3d5959c957a2
Sameer <sameer.raza9200@gmail.com>
2023-09-22 02:48:53 +0500

    Fixing useEffect

--- client/src/components/dashboard/Root_page.jsx
+++ client/src/components/dashboard/Root_page.jsx
@@ -20,91 +20,91 @@ import { MdOutlineManageAccounts } from "react-icons/md"
 import SocketContext from '../../SocketContext'
 
 
-export default function Root_page({ agencyView}) {
+export default function Root_page({ agencyView }) {
     const socket = useContext(SocketContext);
     const location = useLocation()
     const { id } = useParams();
 
     const [messages, setMessages] = useState([]);
 
-    useEffect(()=>{
-      socket.connect()
-      socket.emit('agentConnect', id);
-      socket.on('identify', () => {
-        // socket.emit('joinRoom', 'Support Agents', id);
+    useEffect(() => {
+        socket.connect()
         socket.emit('agentConnect', id);
-      });
-  
-      socket.on('userDisconnect', (socketId) => {
-        socket.emit('leaveRoom', socketId);
-        // setMessages(prevMessages => {
-        //   let updatedMessages = [...prevMessages];
-        //   if (active === updatedMessages.length - 1) {
-        //     setActive(active - 1)
-        //   }
-        //   updatedMessages = updatedMessages.filter(message => message.socketId !== socketId);
-        //   return updatedMessages;
-        // });
-      });
-    })
-  
-    socket.on("Pair with Customer", (id) => {
-      console.log('pairing')
-      socket.emit("joinRoom", id);
-    });
-  
-    socket.on("new message", (data, socketId) => {
-      console.log(data)
-      // console.log('new message received')
-      // Directly using setMessages with function parameter 
-      // to ensure we always work with the most current state
-      setMessages(prevMessages => {
-        const existingIndex = prevMessages.findIndex(message => message.conversationId === data.conversationId);
-  
-        if (existingIndex > -1) {
-          // Existing conversation   
-          // Using spread to make a new copy and mutate that
-          const updatedMessages = [...prevMessages];
-          updatedMessages[existingIndex].conversation.push({
-            sender: data.sender,
-            body: data.body
-          });
-          return updatedMessages;
-        }
-        else {
-          // New conversation
-          const newConversation = {
-            conversationId: data.conversationId,
-            socketId: socketId,
-            new: true,
-            conversation: [{ sender: data.sender, body: data.body }],
-            startTime: new Date()
-          };
-          return [...prevMessages, newConversation];
-        }
-      });
-  
+        socket.on('identify', () => {
+            // socket.emit('joinRoom', 'Support Agents', id);
+            socket.emit('agentConnect', id);
+        });
+
+        socket.on('userDisconnect', (socketId) => {
+            socket.emit('leaveRoom', socketId);
+            // setMessages(prevMessages => {
+            //   let updatedMessages = [...prevMessages];
+            //   if (active === updatedMessages.length - 1) {
+            //     setActive(active - 1)
+            //   }
+            //   updatedMessages = updatedMessages.filter(message => message.socketId !== socketId);
+            //   return updatedMessages;
+            // });
+        });
+
+
+        socket.on("Pair with Customer", (id) => {
+            console.log('pairing')
+            socket.emit("joinRoom", id);
+        });
+
+        socket.on("new message", (data, socketId) => {
+            console.log(data)
+            // console.log('new message received')
+            // Directly using setMessages with function parameter 
+            // to ensure we always work with the most current state
+            setMessages(prevMessages => {
+                const existingIndex = prevMessages.findIndex(message => message.conversationId === data.conversationId);
+
+                if (existingIndex > -1) {
+                    // Existing conversation   
+                    // Using spread to make a new copy and mutate that
+                    const updatedMessages = [...prevMessages];
+                    updatedMessages[existingIndex].conversation.push({
+                        sender: data.sender,
+                        body: data.body
+                    });
+                    return updatedMessages;
+                }
+                else {
+                    // New conversation
+                    const newConversation = {
+                        conversationId: data.conversationId,
+                        socketId: socketId,
+                        new: true,
+                        conversation: [{ sender: data.sender, body: data.body }],
+                        startTime: new Date()
+                    };
+                    return [...prevMessages, newConversation];
+                }
+            });
+        })
       const beforeUnload = (ev) => {
-        ev.preventDefault();
-        // socket.emit('leaveRoom', `Support Agents ${id}`);
-        socket.disconnect();
-      };
-  
-      window.addEventListener('beforeunload', beforeUnload);
-  
-  
-  
-      // Cleanup function to leave room when component unmounts
-      return () => {
-        window.removeEventListener('beforeunload', beforeUnload);
-        // socket.emit('leaveRoom');
-        // socket.disconnect()
-      }
-  
+            ev.preventDefault();
+            // socket.emit('leaveRoom', `Support Agents ${id}`);
+            socket.disconnect();
+        };
+
+        window.addEventListener('beforeunload', beforeUnload);
+
+
+
+        // Cleanup function to leave room when component unmounts
+        return () => {
+            window.removeEventListener('beforeunload', beforeUnload);
+            // socket.emit('leaveRoom');
+            // socket.disconnect()
+        }
+
     }, []);
-  
-  
-    
+
+
+
 
     return (
         <>
@@ -165,7 +165,7 @@ export default function Root_page({ agencyView}) {
                         <Link to={`/chatbot/message-history/${id}`}>
                             <div className='flex items-center gap-2 active:scale-95 cursor-pointer'>
                                 <div>
-                                <RiChatHistoryLine className='text-2xl' />
+                                    <RiChatHistoryLine className='text-2xl' />
                                 </div>
                                 <div>
                                     <h3>Message History</h3>
@@ -212,7 +212,7 @@ export default function Root_page({ agencyView}) {
                     location.pathname === `/chatbot/embed-and-Share/${id}` ? <Embed_and_Share /> : ""
                 }
                 {
-                    location.pathname === `/chatbot/chat-inbox/${id}` ? <Chat_Inbox  messages={messages} setMessages={setMessages} /> : ""
+                    location.pathname === `/chatbot/chat-inbox/${id}` ? <Chat_Inbox messages={messages} setMessages={setMessages} /> : ""
                 }
                 {
                     location.pathname === `/chatbot/message-history/${id}` ? <MessageHistory /> : ""
